b42d2d1a40c666e78939a964adfc0c6fa58cf54e
Joar Løvik Njølstad <njolstad@gmail.com>
2023-01-26 11:54:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var image = document.getElementById("image");
 var prevBtn = document.getElementById("prev-btn");
 var nextBtn = document.getElementById("next-btn");
 var pageName = location.pathname.split("/").pop().replace('.html', '.jpg');
-var images = ["images/benedikteogjoar.jpg", "images/" + pageName];
+var images = ["images/" + pageName", "images/benedikteogjoar.jpg"];
 
 var currentImageIndex = 0;
 
